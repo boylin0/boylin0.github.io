c90c38e6d7b20d54f5a57addb1209c1a20dd4321
BOYI <as20757738@gmail.com>
2021-05-01 17:56:53 +0800

    Update Home

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -49,6 +49,10 @@ class Home extends React.Component {
         wow.sync();
     }
 
+    goMyGithub() {
+        window.location.href = 'https://github.com/boylin0';
+    }
+
     render() {
         return (
             <div className="bg-white pb-5">
@@ -64,7 +68,7 @@ class Home extends React.Component {
                         />
                     </div>
                     <div className="wow fadeIn home-background-text text-white w-100 h-100 p-4" data-wow-duration="3s" data-wow-delay="0.5s">
-                        <div className="d-flex justify-content-center h-100">
+                        <div className="d-flex flex-column justify-content-center h-100">
                             <h1 className="d-block d-sm-block d-md-none font-weight-lighter text-center" style={{ fontSize: '10vw' }}>
                                 <a className="text-white hover-text" style={{ textDecoration: 'none' }} href="https://github.com/boylin0"><i className="fab fa-github"></i> BOYLIN0</a>
                                 <br />
@@ -80,38 +84,43 @@ class Home extends React.Component {
 
                 </div>
 
-                {/* GitHub Stats Card */}
-                <div className="p-3 bg-white">
-                    <div className="row">
-                        <div className="col-12 p-4 text-muted">
-                            <div className="d-flex justify-content-center p-2">
-                                <h3 className="font-weight-light"><i className="fa fa-compass p-1"></i> GitHub Stats Card</h3>
-                            </div>
-                        </div>
+                {/* Application */}
+                <div className="text-center wow fadeIn">
+                    <h3 className="m-3 gradient-text" style={{ fontSize: '2.5rem', fontWeight: '900' }}>My Application</h3>
+                </div>
 
+                {/* Application: LiveABC */}
+                <section className="row m-0 d-flex align-items-center justify-content-center wow fadeIn">
+                    <div className="col-12 col-sm-6 p-3">
+                        <img src={require('../../resource/home-section-liveabc.svg').default}></img>
+                    </div>
+                    <div className="col-12 col-sm-6 p-3 text-center wow fadeIn" data-wow-delay="0.5s">
+                        <div className="p-3">
+                            <h2>LiveABC自動產生解答工具</h2>
+                            <small className="text-muted">(For NPTU only)</small>
+                        </div>
+                        <Link className="btn btn-outline-primary btn-lg" to={"/LiveABC"} style={{ minWidth: '150px' }}><i className="fa fa-arrow-right"></i> 前往</Link>
+
+                    </div>
+                </section>
+                
+                {/* About Me */}
+                <div className="text-center wow fadeIn">
+                    <h3 className="m-3 gradient-text" style={{ fontSize: '2.5rem', fontWeight: '900' }}>About Me</h3>
+                </div>
+
+                {/* About Me: Content */}
+                <section className="p-5 bg-white wow fadeIn">
+                    <div className="row">
                         <div className="col-12 col-lg-6 text-center text-lg-right mb-3">
                             <img style={{ maxWidth: '100%', Width: '100%', height: '100%' }} src={"https://github-readme-stats.vercel.app/api?username=boylin0&count_private=true&show_icons=true&icon_color=fff&bg_color=30,e96443,904e95&title_color=fff&text_color=fff"} />
                         </div>
-
                         <div className="col-12 col-lg-6 text-center text-lg-left mb-3">
                             <img style={{ maxWidth: '100%', Width: '100%', height: '100%' }} src={"https://github-readme-stats.anuraghazra1.vercel.app/api/top-langs/?username=boylin0&layout=compact&bg_color=30,e96443,904e95&title_color=fff&text_color=fff"} />
                         </div>
                     </div>
-                </div>
+                </section>
 
-                {/* Toolbox */}
-                <div className="p-5 bg-white">
-                    <div className="row">
-                        <div className="col-12 p-4 text-muted">
-                            <div className="d-flex justify-content-center p-2">
-                                <h3 className="font-weight-light"><i className="fa fa-tools p-1"></i> Toolbox</h3>
-                            </div>
-                        </div>
-
-                        <Card intro="LiveABC 解答工具 (For NPTU)" src={LiveABC_logo} alt="LiveABC_logo" to="/LiveABC"></Card>
-
-                    </div>
-                </div>
             </div>
 
         );
